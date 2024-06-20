8abbd95945ec1f80ef5a31ade0d1154225748742
sparrowsl <benthorpe.dev@gmail.com>
2024-06-20 15:03:11 +0000

    chore: format & lint with biomejs

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import { stdin as input, stdout as output } from "process"
-import readline from "readline/promises"
+import { stdin as input, stdout as output } from "node:process";
+import readline from "node:readline/promises";
 
 /**
  * Takes a user input from terminal and returns a string
- * @param {string} [prompt=] 
+ * @param {string} [prompt=]
  * @returns {Promise<String>}
  */
 export default async (prompt = "") => {
-	const rl = readline.createInterface({ input, output })
+	const rl = readline.createInterface({ input, output });
 
-	const result = await rl.question(prompt)
-	rl.close()
-	return result
-}
-
+	const result = await rl.question(prompt);
+	rl.close();
+	return result;
+};
