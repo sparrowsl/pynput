d5b409a9c48fa12fbec42ef0c384eabbaebf9428
sparrowsl <benthorpe.dev@gmail.com>
2024-09-05 15:57:03 +0000

    fix: revert to correct code having options

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,17 @@
 /**
  * Takes a user input from terminal and returns a string
  * @param {string} [prompt=]
- * @param {Object} [options={}] - The options for parsing input. Possible properties:
+ * @param {{int?: boolean, float?: boolean}} [options=] - The options for parsing input.
+ * Possible properties:
  *   - { boolean } int: When true, parses the input as an integer.
  *   - { boolean } float: When true, parses the input as a float.
+ *  If both are true, int takes precedence
  * @returns {Promise<String|Number>}
  */
-export function input(prompt?: string, options?: { int?: boolean, float?: boolean }): Promise<string | number>;
+export function input(
+	prompt?: string,
+	options?: {
+		int?: boolean;
+		float?: boolean;
+	},
+): Promise<string | number>;
--- index.js
+++ index.js
@@ -4,31 +4,29 @@ import readline from "node:readline/promises";
 /**
  * Takes a user input from terminal and returns a string
  * @param {string} [prompt=]
- * @param {Object} [options={}] - The options for parsing input: { int: true } or { float: true }
+ * @param {{int?: boolean, float?: boolean}} [options=] - The options for parsing input.
+ * Possible properties:
+ *   - { boolean } int: When true, parses the input as an integer.
+ *   - { boolean } float: When true, parses the input as a float.
+ *  If both are true, int takes precedence
  * @returns {Promise<String|Number>}
  */
-export async function input(prompt = "", options = {}) {
+export async function input(
+	prompt = "",
+	options = { int: false, float: false },
+) {
 	const rl = readline.createInterface({ input: stdin, output });
 
 	const result = await rl.question(prompt);
 	rl.close();
 
-	if (options.int) {
-        const parsed_int = parseInt(result, 10)
-		if (isNaN(parsed_int)) {
-			throw new Error("input is not a valid integer");
-		}
-		return parsed_int;
+	if (options.int && !Number.isNaN(result)) {
+		return Number.parseInt(result);
 	}
 
-	if (options.float) {
-        const parsed_float = parseFloat(result)
-		if (isNaN(parsed_float)) {
-			throw new Error("input is not a valid float");
-		}
-		return parsed_float;
+	if (options.float && !Number.isNaN(result)) {
+		return Number.parseFloat(result);
 	}
-
 
 	return result;
 }
